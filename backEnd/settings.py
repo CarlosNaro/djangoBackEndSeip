f339682f0499e5fb221f4be8c5fa3187d0adccf3
CarlosNaro <elmaximolider75@gmail.com>
2022-11-30 08:59:26 -0500

    mejorando la jwt

--- backEnd/settings.py
+++ backEnd/settings.py
@@ -61,6 +61,7 @@ SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    
 }
 
 #::::::::::::::::::
