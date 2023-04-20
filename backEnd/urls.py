65cf44c13398b22e2cd2511c22220ec43dceeacf
CarlosNaro <elmaximolider75@gmail.com>
2023-04-20 17:35:40 -0500

    eliminar luego

--- backEnd/urls.py
+++ backEnd/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('users/', include('Apps.users.urls')),
     
     # path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
