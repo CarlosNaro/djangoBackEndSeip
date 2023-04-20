d78cf7449c12979e46d55c893c91c3576e2fcc21
CarlosNaro <elmaximolider75@gmail.com>
2023-04-20 18:51:54 -0500

    una nivel antes de la creacion DB Nueva

--- Apps/users/models.py
+++ Apps/users/models.py
@@ -6,31 +6,26 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('El email es obligatorio')
 
-    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
-        now = timezone.now()
-        if not username:
-            raise ValueError(('The given username must be set'))
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email,
-                        is_staff=is_staff, 
-                        is_superuser=is_superuser,   
-                        **extra_fields)
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        return self._create_user(username, email, password, False, False, ' ',
-                                 **extra_fields)
-
-    def create_superuser(self, username, email, password, **extra_fields):
-        user = self._create_user(username, email, password, True, True,
-                                 **extra_fields)
-        user.is_active = True
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+        )
+        user.is_staff = True
         user.save(using=self._db)
         return user
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
