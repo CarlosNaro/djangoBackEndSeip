e5045e1778ab7b21d0f64f9cf05bf50fd50611ff
unknown <elmaximolider75@gmail.com>
2023-08-23 22:52:53 -0500

    cambios regulares

--- Apps/apunte/models.py
+++ Apps/apunte/models.py
@@ -1,77 +1,82 @@
-
 from django.db import models
 
 # Create your models here.
 
-#tabla producto 
+
+# tabla producto
 class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=250, null=True, blank=True)
-    date = models.DateField( auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        text ="{0} {1}"
+        text = "{0} {1}"
         return text.format(self.name, self.description)
 
 
-#tabla Cliente
+# tabla Cliente
 class Client(models.Model):
     name = models.CharField(max_length=250)
     document = models.CharField(max_length=8, null=True, blank=True)
     description = models.CharField(max_length=250, null=True, blank=True)
     phone = models.CharField(max_length=250, null=True, blank=True)
-    date = models.DateField( auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        text = '{0} - {1}'
+        text = "{0} - {1}"
         return text.format(self.name, self.description)
-    
+
     # def __str__(self) -> str:
     #     text = "{0} "
     #     return text.format(self.name)
 
-   
 
-#Tabla Order 
+# Tabla Order
 class Order(models.Model):
     id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
-    subTotal = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, )
-    total = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, )
-    
+    subTotal = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    total = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
     def __str__(self):
         return self.id_client.name
 
 
-    
-# tabla Detail 
+# tabla Detail
 class Detail(models.Model):
-    id_order = models.ForeignKey(Order, on_delete= models.CASCADE, related_name='order_details' )
-    id_product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    id_order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_details"
+    )
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=8, decimal_places=3)
-    price  = models.DecimalField(max_digits=8, decimal_places=3)
-    subTotal = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=3)
+    subTotal = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True
+    )
     total = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
-
-
 
     # def suma(self ):
     #     text = "{0}"
     #     return text.format(round(self.amount * self.price ,2))
-         
+
     # def __str__(self):
     #     text="Nom_Orden: {0} / Product: {1} / Cantidad: {2} / PU: {3} / Total :{4}"
     #     return text.format(self.id_order,self.id_product, self.amount, self.price,self.suma() )
 
- 
-#tabla expenses
+
+# tabla expenses
 class Expenses(models.Model):
     name = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=8, decimal_places=3)
     description = models.CharField(max_length=250, null=True, blank=True)
     date = models.DateField(auto_now_add=True)
-
-
-
-
-
